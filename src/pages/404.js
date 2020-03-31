6ff665df33560e84f1ede197221ba229447f8716
José Cabeda <jecabeda@gmail.com>
2020-03-31 19:14:08 +0100

    Fix to 404 page

--- src/pages/404.js
+++ src/pages/404.js
@@ -1,14 +1,15 @@
 import React from 'react'
+import Layout from '../components/layout'
 import Link from 'gatsby-link'
 
-const NotFoundPage = () => (
-  <div>
+const NotFoundPage = (props) => (
+  <Layout location={props.location}>
     <h1>NOT FOUND</h1>
     <p>You just found a void... Please turn back or go to the &nbsp;
       <Link to="/">
       beginning of times
       </Link>...</p>
-  </div>
+  </Layout>
 )
 
 export default NotFoundPage
