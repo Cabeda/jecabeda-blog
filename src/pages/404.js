f20b54f0e7c91d06c7a6e8cc22cd9a16ad25e647
Cabeda <jecabeda@gmail.com>
2021-12-21 14:53:19 +0000

    Change 404 page

--- src/pages/404.js
+++ src/pages/404.js
@@ -1,16 +1,14 @@
 import React from 'react'
 import Layout from '../components/layout'
 import Link from 'gatsby-link'
+import GIF from './resources/harry-potter-things.gif'
 
 export default function NotFoundPage ({location}) {
   return (
 
     <Layout location={location}>
     <h1>NOT FOUND</h1>
-    <p>You just found a void... Please turn back or go to the &nbsp;
-    <Link to="/">
-    beginning of times
-    </Link>...</p>
+    <img src={GIF} alt="Luna and Harry Potter things"></img>
     </Layout>
     
     )
