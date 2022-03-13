70fee91558e02d1ef79b6e0f7a419ff880beaebc
Cabeda <jecabeda@gmail.com>
2022-03-13 15:10:19 +0000

    Remove logs

--- gatsby-browser.js
+++ gatsby-browser.js
@@ -5,15 +5,11 @@ const upPageCount = (location) => {
     if (!response.ok) {
       throw new Error(`HTTP error! Status: ${response.status}`);
     }
-    console.log("Run api");
     return response.blob();
   });
 };
 
 // Logs when the client route changes
 exports.onRouteUpdate = ({ location, prevLocation }) => {
-  console.log("new pathname", location.pathname);
-  console.log("old pathname", prevLocation ? prevLocation.pathname : null);
-
   upPageCount(location.pathname);
 };
