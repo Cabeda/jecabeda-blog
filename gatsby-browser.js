3f7be1e7ea48ea9c1152fcc764c30f5a3bb26a88
Cabeda <jecabeda@gmail.com>
2022-04-09 18:37:44 +0100

    Make api count a non blocking feature (print log in case of failure)

--- gatsby-browser.js
+++ gatsby-browser.js
@@ -1,12 +1,16 @@
-const axios = require('axios');
+const axios = require("axios");
 
 const upPageCount = (location) => {
-  axios(`/api/count?page=${location}`).then((response) => {
-    if (!response.ok) {
-      throw new Error(`HTTP error! Status: ${response.status}`);
-    }
-    return response.blob();
-  });
+  axios(`/api/count?page=${location}`)
+    .then((response) => {
+      if (!response.ok) {
+        throw new Error(`HTTP error! Status: ${response.status}`);
+      }
+      return response.blob();
+    })
+    .catch((error) => {
+      console.log("Count Error: ", error);
+    });
 };
 
 // Logs when the client route changes
